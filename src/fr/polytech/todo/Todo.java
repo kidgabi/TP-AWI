5fec1439023da3eac09949c6f52d42f1c6cd8fd3
user <gabartigues@gmail.com>
2016-09-23 17:01:01 +0200

    jfjf

---
+++ src/fr/polytech/todo/Todo.java
@@ -0,0 +1,67 @@
+package fr.polytech.todo;
+
+public class Todo {
+	long id;
+	String name;
+	String description;
+	
+	
+	
+	public Todo(long id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	@Override
+	public String toString(){
+		return ("["+id+"] "+name+" : "+description);
+		
+	}
+	
+	public static void main(){
+		Todo todo = new Todo(1, "test", "blabla");
+		System.out.println(todo.toString());
+		
+	}
+
+}
